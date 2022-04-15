18289c4776962e193d56a3b29b525eae576405e3
Zachary Sirna <zsirna@yahoo.com>
2022-04-15 13:51:19 -0400

    adding components to start structuring app

---
+++ src/Components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+  <div >
+    <h1>Horror Movies</h1>
+    <p>A website for listing top horror movies and highly reccomended lesser seen horrors. </p>
+  </div>
+  )
+}
+
+export default Home
