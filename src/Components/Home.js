852d9528002821b9f24970c8df17eb7b8ca525f9
Zachary Sirna <zsirna@yahoo.com>
2022-04-15 13:55:22 -0400

    adding material UI dependencies for styling for later use

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
   <div >
     <h1>Horror Movies</h1>
     <p>A website for listing top horror movies and highly reccomended lesser seen horrors. </p>
+    <p>👻</p>
   </div>
   )
 }
