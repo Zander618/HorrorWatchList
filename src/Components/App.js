a5cb0bd72567db4d1f22ef4166f57b5e6355a3cc
Zachary Sirna <zsirna@yahoo.com>
2022-04-15 14:29:02 -0400

    styling NavBar

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
 import React from "react"
 import Home from "./Home";
+import NavBar from "./NavBar";
 
 const App = () => {
   return (
     <div>
+      <NavBar />
       <Home />
     </div>
   );
