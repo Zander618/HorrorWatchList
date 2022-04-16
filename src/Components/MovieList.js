9c2d8ffaf23eda67770c5d18325ba9dcd4e8311a
Zachary Sirna <zsirna@yahoo.com>
2022-04-16 10:20:06 -0400

    creating fetch request to render  movies to the DOM

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const MovieList = () => {
+  const [movies, setMovies] = useState(null)
+
+  useEffect(() => {
+    fetch ("http://localhost:3001/movies")
+    .then(resp => resp.json())
+    .then((data) => setMovies(data))
+  }, [])
+
+
   
+  const movieList = movies.map((movie) => {
+    return(
+      <li key={movie.id}>
+        <h3>{movie.title}</h3>
+        <img src={movie.cover} alt={movie.title}/>
+        <p>Release: {movie.release}</p>
+        <p>Runtime: {movie.length}</p>
+        <button>Add to my watch list</button>
+        <button>More info</button>
+      </li>
+    )
+  })
   
   return (
     <div>
       <h1>Movie List</h1>
       <ul>
-        <li>where movies go</li>
+        {movieList}
       </ul>
     </div>
   )
